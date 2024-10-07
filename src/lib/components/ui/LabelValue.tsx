b9407247afaf2b736839f72f13f3ac9cf101c4b1
leekimah <mikhailsebua@gmail.com>
2024-10-07 16:43:52 +0800

    Added Service application address, applicant information, attached documents in Application Summary

--- src/lib/components/ui/LabelValue.tsx
+++ src/lib/components/ui/LabelValue.tsx
@@ -3,23 +3,56 @@ import React from 'react';
 type LabelValueProps = {
   orientation?: 'horizontal' | 'vertical';
   label: string;
-  value: string | number | null;
+  value: string | number | null | undefined;
   id: string;
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 };
 
 export const LabelValue = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement> & LabelValueProps>(
-  ({ id, label, value, orientation = 'horizontal', ...props }, ref) => {
+  ({ id, label, value, orientation = 'horizontal', size = 'md', ...props }, ref) => {
     return (
       <>
         {orientation === 'horizontal' ? (
-          <div {...props} className={`${props.className} grid grid-cols-2 gap-2`} ref={ref}>
+          <div
+            {...props}
+            className={`${props.className} ${
+              size === 'xs'
+                ? 'text-xs'
+                : size === 'sm'
+                ? 'text-sm'
+                : size === 'md'
+                ? 'text-base'
+                : size === 'lg'
+                ? 'text-lg'
+                : size === 'xl'
+                ? 'text-xl'
+                : 'text-base'
+            } grid grid-cols-2 gap-2 hover:underline font-sans`}
+            ref={ref}
+          >
             <label htmlFor={id} className="text-gray-500">
               {label}
             </label>
             <div className="font-normal text-gray-900">{value}</div>
           </div>
         ) : orientation === 'vertical' ? (
-          <div {...props} className={`${props.className} grid grid-rows-2 grid-cols-1 gap-0`} ref={ref}>
+          <div
+            {...props}
+            className={`${props.className} ${
+              size === 'xs'
+                ? 'text-xs'
+                : size === 'sm'
+                ? 'text-sm'
+                : size === 'md'
+                ? 'text-base'
+                : size === 'lg'
+                ? 'text-lg'
+                : size === 'xl'
+                ? 'text-xl'
+                : 'text-base'
+            } grid grid-rows-2 grid-cols-1 gap-0 hover-underline font-sans`}
+            ref={ref}
+          >
             <label htmlFor={id} className="text-gray-500">
               {label}
             </label>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -62,6 +62,28 @@ const config: Config = {
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
       },
+      keyframes: {
+        'accordion-down': {
+          from: {
+            height: '0',
+          },
+          to: {
+            height: 'var(--radix-accordion-content-height)',
+          },
+        },
+        'accordion-up': {
+          from: {
+            height: 'var(--radix-accordion-content-height)',
+          },
+          to: {
+            height: '0',
+          },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
     },
     animation: {
       blob: 'blob 7s infinite',
@@ -89,6 +111,14 @@ const config: Config = {
         to: {
           transform: 'rotate(360deg)',
         },
+      },
+      'accordion-down': {
+        from: { height: '0' },
+        to: { height: 'var(--radix-accordion-content-height)' },
+      },
+      'accordion-up': {
+        from: { height: 'var(--radix-accordion-content-height)' },
+        to: { height: '0' },
       },
     },
   },
